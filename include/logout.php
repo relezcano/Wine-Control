6bb9124dacb6f04b8419c1c2a93fec3bb05ebea5
Ramiro Lezcano <relezcano369@gmail.com>
2018-03-29 23:11:54 -0300

    Delete logout.php

--- include/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-unset($_SESSION['login']);
-unset($_SESSION['email']);
-unset($_SESSION['name']);
-unset($_SESSION['ingresoAdmin']);
-
-header('Location: ../index.php?alt=6');
- ?>
